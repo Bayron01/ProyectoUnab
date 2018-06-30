5c9b4367538963ed000012363ce1d1a3c89c72d3
Bayron01 <31601396+Bayron01@users.noreply.github.com>
2018-06-30 00:01:33 -0400

    nuevo

--- Universidad/urls.py
+++ Universidad/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
 	
 	path('paginaprincipal', views.paginaprincipal, name='pagina_principal'),
     path('agregarprofesor', views.agregarprofesor, name='agregar_profesor'),
+    path('listarprofesor', views.listarprofesor, name='listar_profesor'),
+    path('detallarprofesor/<int:profesor_id>', views.detallarprofesor, name='detallar_profesor'),
+    path('editarprofesor/<int:profesor_id>', views.editarprofesor, name='editar_profesor'),
 
 ]
--- Universidad/views.py
+++ Universidad/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from Universidad.models import Profesor
 from Universidad.forms import ProfesorForm
 
@@ -9,7 +10,7 @@ def paginaprincipal(request):
 	template_name = 'principal/pagina_principal.html'
 	return render(request, template_name, data)
 
-	
+
 def agregarprofesor(request):
     data = {}
     if request.method == "POST":
@@ -19,11 +20,41 @@ def agregarprofesor(request):
             # aca el formulario valido
             data['form'].save()
 
-            #return redirect('listar_jugador')
+            return redirect('listar_profesor')
 
     else:
         data['form'] = ProfesorForm()
    
+
+    template_name = 'profesor/agregar_profesor.html'
+    return render(request, template_name, data)
+
+def listarprofesor(request):
+    data = {}
+    
+    data['object_list'] = Profesor.objects.all().order_by('id')
+
+    template_name = 'profesor/listar_profesor.html'
+    return render(request, template_name, data)
+
+def detallarprofesor(request, profesor_id):
+    data = {}
+    data['profesor'] = Profesor.objects.get(pk=profesor_id)
+    template_name = 'profesor/detallar_profesor.html'
+    return render(request, template_name, data)
+
+def editarprofesor(request, profesor_id):
+    data = {}
+    profesor = Profesor.objects.get(pk=profesor_id)
+    if request.method == "GET":
+        data['form'] = ProfesorForm(instance= profesor)
+
+    else:
+        data['form'] = ProfesorForm(request.POST, request.FILES, instance= profesor)  
+        if data['form'].is_valid():
+            # aca el formulario valido
+            data['form'].save()
+            return redirect('listar_profesor') 
 
     template_name = 'profesor/agregar_profesor.html'
     return render(request, template_name, data)
